1f1ba2186138eea14702b26ee94216053e20be94
Omega Mputu <kutekemeni93@gmail.com>
2023-10-30 08:32:59 +0100

    Dashboard widgets created

---
+++ app/Filament/Widgets/BlogPostOverview.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Post;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class BlogPostOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+            Stat::make('Draft', Post::query()->where('status', 'draft')->count())
+                ->description('Nomber of draft post'),
+            Stat::make('Reviewing', Post::query()->where('status', 'reviewing')->count())
+                ->description('Number of post revised')
+                ->color('info'),
+            Stat::make('Published', Post::query()->where('status', 'published')->count())
+                ->description('Number of post published')
+                ->color('success'),
+        ];
+    }
+}
